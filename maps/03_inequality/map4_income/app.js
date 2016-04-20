e06f39bb44c99ed2fcdf2ba67ce493877825a828
Ryan <ryancooper729@gmail.com>
2016-04-19 20:08:53 -0400

    add single variable maps with tooltips

--- maps/03_inequality/map4_income/app.js
+++ maps/03_inequality/map4_income/app.js
@@ -1,53 +1,17 @@
 window.onload = function() {
   var attributeStyleInfo = {
-    "gini_10":{
-      colorRamp: ['#edf8e9','#bae4b3','#74c476','#31a354','#006d2c'],
-      rangeText: ['<0.35','0.35-0.4','0.4-0.45','0.45-0.5','>=0.5']
-    },
-    "eduindex_10":{
-      colorRamp: ['#eff3ff','#bdd7e7','#6baed6','#3182bd','#08519c'],
-      rangeText: ['<3','3-4','4-5','5-6','>=6']
-    },
     "incomeindex_10":{
       colorRamp: ['#fee5d9','#fcae91','#fb6a4a','#de2d26','#a50f15'],
       rangeText: ['<2','2-4','4-6','6-8','>=8']
     }
   }
-  var starterChip = attributeStyleInfo.gini_10.colorRamp;
-  var starterLabel = attributeStyleInfo.gini_10.rangeText;
+  var starterChip = attributeStyleInfo.incomeindex_10.colorRamp;
+  var starterLabel = attributeStyleInfo.incomeindex_10.rangeText;
   for (var i = 0; i < 5; i++){
     $('#legend-attribute-chip-' + (i+1).toString()).css('background-color', starterChip[i]);
     $('#legend-attribute-label-' + (i+1).toString()).html(starterLabel[i]);
   }
 
-  function createSelector(layer)  {
-    var cartocss = "";
-    var $options = $("#menu").find("ul li a");
-    $options.click(function(e) {
-      var $button = $(e.target);
-      var selected = $button.attr('data')
-      var selectedVar = $button.attr('value');
-
-       
-      var chip = attributeStyleInfo[selected].colorRamp;
-      var label = attributeStyleInfo[selected].rangeText;
-      $('.legend-attribute-label').html('');
-      for (var i = 0; i < 5; i++){
-        $('#legend-attribute-chip-' + (i+1).toString()).css('background-color', chip[i]);
-        $('#legend-attribute-label-' + (i+1).toString()).html(label[i]);
-      }
-
-      if($(this).hasClass('selectable')){
-        $options.removeClass('selected');
-        $button.addClass('selected');
-        $('.title-variable').html(selectedVar);
-        $('.legend-attribute-title').html(selectedVar);
-        cartocss = $('#' + selected).text();
-        layer.setCartoCSS(cartocss);
-      }
-    })
-  }
-  
   var usCounties = 'adhp_counties_10';
 
   var layerSource = {
@@ -55,8 +19,8 @@ window.onload = function() {
     type: 'cartodb',
     sublayers: [{
       sql: "SELECT * FROM " + usCounties,
-      cartocss: $('#gini_10').text(),
-      interactivity: "cartodb_id, the_geom, county, state, gini_10, eduindex_10, incomeindex_10"
+      cartocss: $('#incomeindex_10').text(),
+      interactivity: "cartodb_id, the_geom, county, state, incomeindex_10"
     }]
   }
 
@@ -67,6 +31,8 @@ var sublayer;
 var map = new L.Map('map', {
   center: [39.828328, -98.579416],
   zoom: 4,
+  maxZoom:9,
+  minZoom:3,
   zoomControl: false
 });
 
@@ -86,7 +52,7 @@ cartodb.createLayer(map, layerSource)
       cartodb.vis.Vis.addInfowindow(
         map,
         sublayer,
-        ['county','state','gini_10','eduindex_10','incomeindex_10'], 
+        ['county','state','incomeindex_10'], 
         {
           infowindowTemplate: $('#infowindow_template').html(),
           templateType: 'mustache'
@@ -99,8 +65,6 @@ cartodb.createLayer(map, layerSource)
         postition: 'bottom|right',
         fields: [{county: 'county'}]
       });
-      // sublayer.infowindow.set('template', $('#infowindow_template').html());
-      createSelector(sublayer);
 
       cartodb.createLayer(map, countyCoal)
         .addTo(map, 1)
